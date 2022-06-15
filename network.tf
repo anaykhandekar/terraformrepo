e130df440d51695d1fc60f7546f8447dd2cc3206
Anay Khandekar <anaykhandekar@Anays-MacBook-Pro.local>
2022-06-15 14:12:20 -0500

    Terraform first push

---
+++ network.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source                                 = "terraform-google-modules/network/google//modules/vpc"
+  project_id                             = "k8s-tutorial-proj0608"
+  network_name                           = "ignite-vpc"
+  routing_mode                           = "GLOBAL"
+  shared_vpc_host                        = false
+  delete_default_internet_gateway_routes = false
+}
