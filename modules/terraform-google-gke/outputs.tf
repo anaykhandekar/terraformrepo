eb13795760697799898ba98171dcbadae418df22
Anay Khandekar <anaykhandekar@Anays-MacBook-Pro.local>
2022-06-16 13:57:33 -0500

    bunch of modules

---
+++ modules/terraform-google-gke/outputs.tf
@@ -0,0 +1,10 @@
+
+# output "primary_cluster_name" {
+#   description = "GKE cluster name"
+#   value       = module.gke[0].name
+# }
+
+# output "failover_cluster_name" {
+#   description = "GKE failover cluster name"
+#   value       = module.failover_gke[0].name
+# }
--- network.tf
+++ network.tf
@@ -1,13 +1,8 @@
 module "vpc" {
   source                                 = "terraform-google-modules/network/google//modules/vpc"
-  project_id                             = "k8s-tutorial-proj0608"
-  network_name                           = "ignite-vpc"
-  routing_mode                           = "GLOBAL"
-  shared_vpc_host                        = false
-  delete_default_internet_gateway_routes = false
-}
-
-
-output "test" {
-  value = "test_output"
+  project_id                             = var.gcp_project_id
+  network_name                           = var.network_name
+  routing_mode                           = var.routing_mode
+  shared_vpc_host                        = var.shared_vpc_host
+  delete_default_internet_gateway_routes = var.delete_default_internet_gateway_routes
 }
